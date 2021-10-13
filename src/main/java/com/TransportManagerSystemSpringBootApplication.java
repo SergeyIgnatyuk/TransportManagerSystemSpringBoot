5be6a8e726b3508a0a88104265d5422fc1e78441
sergey <signatuk89@gmail.com>
2021-10-13 21:25:24 +0300

    Initial commit

---
+++ src/main/java/com/TransportManagerSystemSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportManagerSystemSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransportManagerSystemSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/rest/UserRestController.java
@@ -0,0 +1,43 @@
+package com.rest;
+
+import com.model.User;
+import com.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+@Api(value="management system", tags="Operations on users")
+public class UserRestController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserRestController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    @PreAuthorize("hasAnyAuthority('developers:read')")
+    @ApiOperation(value = "View a list of users")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved list"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    })
+    public ResponseEntity<List<User>> getAllUsers() {
+        return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
+    }
+}
